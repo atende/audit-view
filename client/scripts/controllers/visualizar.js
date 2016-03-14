0b95fc4c9c4336d7d1b54b10791d5af2ff99b601
Claudinei Mendes <claudinei.mendes@sga.pucminas.br>
2016-03-14 10:30:57 -0300

    Incluindo views no projeto revisado.

--- client/scripts/controllers/visualizar.js
+++ client/scripts/controllers/visualizar.js
@@ -7,7 +7,65 @@
  * # VisualizarCtrl
  * Controller of the auditionApp
  */
-angular.module('auditionApp')
-  .controller('VisualizarCtrl', function () {
+
+var app = angular.module('auditionApp');
+
+app.controller('VisualizarCtrl', function ($scope) {
+
+
+    $scope.logSelecionado = null;
+    $scope.indexSelecionado = null;
+
+    $scope.clickTable = function(log, index) {
+      $scope.logSelecionado = log;
+      $scope.indexSelecionado = index;
+    }
+
+    $scope.pesquisar = function(log) {
+      if ($scope.pesquisa) {
+        return log.aplicacao.indexOf($scope.pesquisa) == 0 ||
+            log.usuario.indexOf($scope.pesquisa) == 0 ||
+            log.acao.indexOf($scope.pesquisa) == 0 ||
+            log.datas.indexOf($scope.pesquisa) == 0 ||
+            log.ip.indexOf($scope.pesquisa) == 0 ||
+            log.criticidade.indexOf($scope.pesquisa) == 0;
+      }
+      return true;
+    };
+
+    $scope.logs = [
+      {
+        aplicacao: "Mapa PUC",
+        usuario: "Anderson",
+        acao: "teste",
+        datas: "10/03/2016 : 10:53",
+        ip: "192.168.254.254",
+        criticidade: "altawa"
+      },
+      {
+        aplicacao: "Audit-Web",
+        usuario: "Andersonss",
+        acao: "teste",
+        datas: "10/03/2016 : 10:53",
+        ip: "192.168.254.254",
+        criticidade: "alta"
+      },
+      {
+        aplicacao: "SGL",
+        usuario: "Anderson",
+        acao: "teste",
+        datas: "10/03/2016 : 10:53",
+        ip: "192.168.255.254",
+        criticidade: "alta"
+      },
+      {
+        aplicacao: "SGI",
+        usuario: "Anderson",
+        acao: "teste",
+        datas: "10/03/2016 : 10:53",
+        ip: "192.167.254.254",
+        criticidade: "alta"
+      }
+    ]
 
   });
