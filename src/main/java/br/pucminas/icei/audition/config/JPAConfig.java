ad73f9a3ac9de817b4c7327beae08986605bceed
Bruno Henrique Damato <bruno.damato@sga.pucminas.br>
2016-03-17 08:43:48 -0300

    AUDIT-4_Configurando o pool de conexão HikariCP

---
+++ src/main/java/br/pucminas/icei/audition/config/JPAConfig.java
@@ -0,0 +1,23 @@
+package br.pucminas.icei.audition.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+/**
+ * Created by bruno on 17/03/16.
+ */
+public class JPAConfig {
+    @Configuration
+    @ConfigurationProperties(prefix = "params.datasource")
+    public class JPAConfig extends HikariConfig {//dependência de compile('com.zaxxer:HikariCP:2.4.4') ou outro a ser importado?
+        //A configuração deve ser realizada em apllication-dev.properties ou deve se criar um somente para ele?
+        @Bean
+        public DataSource dataSource() throws SQLException {
+            return new HikariDataSource(this);
+        }
+    }
+}
