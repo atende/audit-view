03f22b225e470df0ef83eedb58445e9f7731f99f
Claudinei Mendes <claudinei.mendes@sga.pucminas.br>
2016-03-18 10:00:39 -0300

    Incluindo pesquisa melhorada e filtro.

--- client/scripts/controllers/visualizar.js
+++ client/scripts/controllers/visualizar.js
@@ -14,6 +14,9 @@ app.controller('VisualizarCtrl', function ($scope) {
 
     $scope.filtros = [];
 
+    $scope.criticidades = ["Alta", "Normal", "Baixa"];
+    $scope.aplicacoes = ["SGI", "Mapa PUC", "SGL", "AUDIT"];
+
     $scope.logSelecionado = null;
     $scope.indexSelecionado = null;
 
@@ -28,55 +31,56 @@ app.controller('VisualizarCtrl', function ($scope) {
     $scope.pesquisar = function(log) {
       if ($scope.pesquisa) {
 
-        if($scope.radio_filtro == "aplicacao"){
-          return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
+        var resp = false;
+
+        if($scope.radio_filtro == "usuario"){
+          resp = log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
         }
-        else if($scope.radio_filtro == "usuario"){
-          return log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
-        }
+
+        //if($scope.radio_filtro == "usuario"){
+        //
+        //
+        //  return log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 &&
+        //         log.aplicacao.indexOf($scope.aplicacaoSelecionada) == 0 &&
+        //         log.criticidade.indexOf($scope.criticidadeSelecionada) == 0;
+        //
+        //}
         else if($scope.radio_filtro == "acao"){
-          return log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
+          resp = log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
         }
         else if($scope.radio_filtro == "id"){
-          return log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
+          resp = log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
         }
         else if($scope.radio_filtro == "datas"){
-          return log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
+          resp = log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
         }
         else if($scope.radio_filtro == "ip"){
-          return log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
+          resp = log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
         }
-        else if($scope.radio_filtro == "criticidade"){
-          return log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
-        }else{
-            return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-            log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0;
-        }
-        //if($scope.filtroPopular === "todos"){
-        //  return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
-        //  log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0
-        //
-        //}else{
-        //  return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "aplicacao" ||
-        //    log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "usuario" ||
-        //    log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "acao" ||
-        //    log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "id" ||
-        //    log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "datas" ||
-        //    log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "ip" ||
-        //    log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "criticidade"
-        //}
 
+        if($scope.criticidadeSelecionada != undefined){
+          console.log(resp);
+          resp = resp && log.criticidade.indexOf($scope.criticidadeSelecionada) == 0;
+        }
+        if($scope.aplicacaoSelecionada != undefined){
+          console.log(resp);
+          resp = resp && log.aplicacao.indexOf($scope.aplicacaoSelecionada) == 0;
+        }
+
+        return resp;
+
+
+      }else if($scope.aplicacaoSelecionada != undefined && $scope.criticidadeSelecionada == undefined){
+        return log.aplicacao.toLowerCase().indexOf($scope.aplicacaoSelecionada.toLowerCase()) == 0;
       }
+      else if($scope.criticidadeSelecionada != undefined && $scope.aplicacaoSelecionada == undefined){
+        return log.criticidade.toLowerCase().indexOf($scope.criticidadeSelecionada.toLowerCase()) == 0;
+      }
+      else if($scope.criticidadeSelecionada != undefined && $scope.aplicacaoSelecionada != undefined){
+        return log.aplicacao.toLowerCase().indexOf($scope.aplicacaoSelecionada.toLowerCase()) == 0 &&
+               log.criticidade.toLowerCase().indexOf($scope.criticidadeSelecionada.toLowerCase()) == 0;
+      }
+
       return true;
     };
 
@@ -88,7 +92,7 @@ app.controller('VisualizarCtrl', function ($scope) {
         id: "12341",
         datas: "10/03/2016 : 10:53",
         ip: "192.168.254.254",
-        criticidade: "altawa",
+        criticidade: "Alta",
         descricao: "bhsadgjkdsa"
       },
       {
@@ -98,7 +102,7 @@ app.controller('VisualizarCtrl', function ($scope) {
         id: "12342",
         datas: "11/03/2016 : 10:53",
         ip: "192.168.254.254",
-        criticidade: "altawa",
+        criticidade: "Alta",
         descricao: ""
       },
       {
@@ -108,7 +112,7 @@ app.controller('VisualizarCtrl', function ($scope) {
         id: "12343",
         datas: "12/03/2016 : 10:53",
         ip: "192.168.254.254",
-        criticidade: "altawa",
+        criticidade: "Alta",
         descricao: ""
       },
       {
@@ -118,12 +122,32 @@ app.controller('VisualizarCtrl', function ($scope) {
         id: "12344",
         datas: "13/03/2016 : 10:53",
         ip: "192.168.254.254",
-        criticidade: "altawa",
+        criticidade: "Alta",
         descricao: ""
       }
     ]
 
   });
+
+
+//if($scope.filtroPopular === "todos"){
+//  return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 ||
+//  log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0
+//
+//}else{
+//  return log.aplicacao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "aplicacao" ||
+//    log.usuario.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "usuario" ||
+//    log.acao.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "acao" ||
+//    log.id.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "id" ||
+//    log.datas.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "datas" ||
+//    log.ip.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "ip" ||
+//    log.criticidade.toLowerCase().indexOf($scope.pesquisa.toLowerCase()) == 0 && $scope.filtroPopular === "criticidade"
+//}
 
 /*
  $scope.incluirFiltro = function(filtro) {
