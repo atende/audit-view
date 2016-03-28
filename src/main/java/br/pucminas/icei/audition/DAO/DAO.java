ae74acd9841a31faf61bdf2ebe521afbd5cfe1df
Bruno Henrique Damato <bruno.damato@sga.pucminas.br>
2016-03-28 08:58:03 -0300

    Modificacoes JPA e DAO

---
+++ src/main/java/br/pucminas/icei/audition/DAO/DAO.java
@@ -0,0 +1,7 @@
+package br.pucminas.icei.audition.DAO;
+
+/**
+ * Created by bruno on 28/03/16.
+ */
+public class DAO {
+}
---
+++ src/main/java/br/pucminas/icei/audition/business/entity/Logs.java
@@ -0,0 +1,7 @@
+package br.pucminas.icei.audition.business.entity;
+
+/**
+ * Created by bruno on 28/03/16.
+ */
+public class Logs {
+}
