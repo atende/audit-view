5d76066ebe8b9c8af973e8acfd30c3d53d09f38b
Giovanni Silva <giovanni@atende.info>
2016-09-24 23:40:35 -0300

    test pass

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -32,7 +32,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome_No_Sandbox'],
+    // you can define custom flags
+    customLaunchers: {
+      Chrome_No_Sandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
