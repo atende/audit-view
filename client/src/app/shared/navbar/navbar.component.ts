a91ab892174a261f3defccd98f7cb52446c2e77e
Giovanni Silva <giovanni@pucminas.br>
2016-09-26 13:58:25 -0300

    app-navbar e app-toolbar

--- client/src/app/shared/navbar/navbar.component.ts
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
  * This class represents the navigation bar component.
  */
 @Component({
-  selector: 'sd-navbar',
+  selector: 'app-navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.css'],
 })
--- client/src/app/shared/toolbar/toolbar.component.ts
+++ client/src/app/shared/toolbar/toolbar.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
  * This class represents the toolbar component.
  */
 @Component({
-  selector: 'sd-toolbar',
+  selector: 'app-toolbar',
   templateUrl: 'toolbar.component.html',
   styleUrls: ['toolbar.component.css']
 })
