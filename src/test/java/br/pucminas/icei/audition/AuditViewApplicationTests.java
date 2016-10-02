0ecdb6ec2d9e4eb0a811352b82285c29e18740e2
Giovanni Silva <giovanni@atende.info>
2016-10-01 23:27:05 -0300

    #6 Try build with wercker

--- src/test/java/br/pucminas/icei/audition/AuditViewApplicationTests.java
+++ src/test/java/br/pucminas/icei/audition/AuditViewApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AuditViewApplication.class)
 @WebAppConfiguration
+@ActiveProfiles({"ci","test"})
 public class AuditViewApplicationTests {
 
 	@Test
