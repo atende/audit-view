0c56ac54248f0ebd3e740c9826ddc1ed1e165aa0
Federico Jansen <federicojansen@gmail.com>
2017-04-06 12:50:41 -0300

    build.prod pass

--- client/src/app/+home/home.module.ts
+++ client/src/app/+home/home.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [CommonModule, SharedModule],
+    imports: [SharedModule],
     declarations: [HomeComponent],
     exports: [HomeComponent],
 })
