a0940ffdadf03765538fc0b1fe504ab53acdba25
Giovanni Silva <giovanni@atende.info>
2017-04-07 10:39:40 -0300

    calendar module import

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { ToolbarComponent } from './toolbar/index';
 import { NavbarComponent } from './navbar/index';
+import { CalendarModule } from 'primeng/primeng';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
