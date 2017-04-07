69eede806e4eff5c9de8a1fbd27f26a2271396c0
Giovanni Silva <giovanni@atende.info>
2017-04-07 08:57:49 -0300

    #8 primeng setup

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,12 +2,9 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 import { ToolbarComponent } from './toolbar/index';
 import { NavbarComponent } from './navbar/index';
-
-import { CalendarModule } from "primeng/primeng"
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
